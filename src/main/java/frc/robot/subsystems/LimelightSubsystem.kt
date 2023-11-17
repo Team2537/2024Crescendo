a460a03e86fd31443af08ddf3721adb2fac133d4
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-16 20:38:29 -0500

    added getters for the limelight variables

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -9,14 +9,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 object LimelightSubsystem : SubsystemBase() {
 
+    // NetworkTableEntry objects for getting data from the Limelight
     private var tx: NetworkTableEntry
     private var ty: NetworkTableEntry
     private var ta: NetworkTableEntry
     private var tv: NetworkTableEntry
     private var ts: NetworkTableEntry
 
+    // NetworkTable object for getting data from the Limelight
     private var table: NetworkTable
 
+    // Variables for storing data from the Limelight
     private var xOffset: Double = 0.0
     private var yOffset: Double = 0.0
     private var area: Double = 0.0
@@ -24,24 +27,49 @@ object LimelightSubsystem : SubsystemBase() {
     private var targetVisible: Boolean = false
 
     init {
+        // Get the default NetworkTable for the Limelight
         table = NetworkTableInstance.getDefault().getTable("limelight")
 
+        // Get the NetworkTableEntry objects for the Limelight
         tx = table.getEntry("tx")
         ty = table.getEntry("ty")
         ta = table.getEntry("ta")
         tv = table.getEntry("tv")
         ts = table.getEntry("ts")
 
+        // Create a Shuffleboard tab for the Limelight
         val visionTab: ShuffleboardTab = Shuffleboard.getTab("Vision")
 
 
     }
 
     override fun periodic() {
+        // Update the data with the latest values from the Limelight
         xOffset = tx.getDouble(0.0)
         yOffset = ty.getDouble(0.0)
         area = ta.getDouble(0.0)
         skew = ts.getDouble(0.0)
-        targetVisible = tv.getBoolean(false)
+        targetVisible = tv.getDouble(0.0) == 1.0
+    }
+
+    // Getters for the data from the Limelight
+    fun getXOffset(): Double {
+        return xOffset
+    }
+
+    fun getYOffset(): Double {
+        return yOffset
+    }
+
+    fun getArea(): Double {
+        return area
+    }
+
+    fun getSkew(): Double {
+        return skew
+    }
+
+    fun isTargetVisible(): Boolean {
+        return targetVisible
     }
 }
