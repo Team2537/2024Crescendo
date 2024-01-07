347f6b92db22e20dc4a27499cb1797f6a5e144a0
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-01-07 08:20:56 -0500

    Revert "Dev into Limelight Helper Classes (Again) (#23)"
    This reverts commit b4f90213838cd0282c0459fe4c3dfbb564501269.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -1,81 +1,25 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.networktables.NetworkTable
-import edu.wpi.first.networktables.NetworkTableEntry
 import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
-import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.Limelight
 
 object LimelightSubsystem : SubsystemBase() {
-    // NetworkTableEntry objects for getting data from the Limelight
-    private var tx: NetworkTableEntry
-    private var ty: NetworkTableEntry
-    private var ta: NetworkTableEntry
-    private var tv: NetworkTableEntry
-    private var ts: NetworkTableEntry
 
-    // NetworkTable object for getting data from the Limelight
-    private var table: NetworkTable
-    private var visionTab: ShuffleboardTab
+    private val limelight : Limelight = Limelight(NetworkTableInstance.getDefault().getTable("limelight"))
 
-    // Variables for storing data from the Limelight
-    private var xOffset: Double = 0.0
-    private var yOffset: Double = 0.0
-    private var area: Double = 0.0
-    private var skew: Double = 0.0
-    private var targetVisible: Boolean = false
+    val xOffset: Double
+        get() = limelight.xOffset
 
-    init {
-        // Get the default NetworkTable for the Limelight
-        table = NetworkTableInstance.getDefault().getTable("limelight")
+    val yOffset: Double
+        get() = limelight.yOffset
 
-        // Get the NetworkTableEntry objects for the Limelight
-        tx = table.getEntry("tx")
-        ty = table.getEntry("ty")
-        ta = table.getEntry("ta")
-        tv = table.getEntry("tv")
-        ts = table.getEntry("ts")
+    val area: Double
+        get() = limelight.area
 
-        visionTab = Shuffleboard.getTab("Vision")
+    val skew: Double
+        get() = limelight.skew
 
-        visionTab.addDouble("X Offset") { xOffset }
-        visionTab.addDouble("Y Offset") { yOffset }
-        visionTab.addDouble("Area") { area }
-        visionTab.addDouble("Skew") { skew }
-        visionTab.addBoolean("Target Visible") { targetVisible }
-
-        // Create a Shuffleboard tab for the Limelight
-        val visionTab: ShuffleboardTab = Shuffleboard.getTab("Vision")
-    }
-
-    override fun periodic() {
-        // Update the data with the latest values from the Limelight
-        xOffset = tx.getDouble(0.0)
-        yOffset = ty.getDouble(0.0)
-        area = ta.getDouble(0.0)
-        skew = ts.getDouble(0.0)
-        targetVisible = tv.getDouble(0.0) == 1.0
-    }
-
-    // Getters for the data from the Limelight
-    fun getXOffset(): Double {
-        return xOffset
-    }
-
-    fun getYOffset(): Double {
-        return yOffset
-    }
-
-    fun getArea(): Double {
-        return area
-    }
-
-    fun getSkew(): Double {
-        return skew
-    }
-
-    fun isTargetVisible(): Boolean {
-        return targetVisible
-    }
+    val targetVisible: Boolean
+        get() = limelight.targetVisible
 }
---
+++ src/main/java/lib/Limelight.kt
@@ -0,0 +1,91 @@
+package lib
+
+import edu.wpi.first.networktables.NetworkTable
+import edu.wpi.first.networktables.NetworkTableEntry
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
+
+/**
+ * Represents a single limelight unit (vision).
+ *
+ * The limelight gets its data from a [NetworkTable].
+ * [This class][Limelight] allows simple access to
+ * various tables through more descriptive name and
+ * properties rather than raw [NetworkTable.getEntry].
+ * It also manages its own [ShuffleboardTab] to
+ * display information about the limelight unit.
+ *
+ * @author Matthew Clark
+ */
+class Limelight(table: NetworkTable) {
+    // NetworkTableEntry objects for getting data from the Limelight
+    private var tx: NetworkTableEntry
+    private var ty: NetworkTableEntry
+    private var ta: NetworkTableEntry
+    private var tv: NetworkTableEntry
+    private var ts: NetworkTableEntry
+
+    private var visionTab: ShuffleboardTab
+
+    init {
+        // Get the NetworkTableEntry objects for the Limelight
+        tx = table.getEntry("tx")
+        ty = table.getEntry("ty")
+        ta = table.getEntry("ta")
+        tv = table.getEntry("tv")
+        ts = table.getEntry("ts")
+
+        visionTab = Shuffleboard.getTab("Vision")
+
+        visionTab.addDouble("X Offset") { xOffset }
+        visionTab.addDouble("Y Offset") { yOffset }
+        visionTab.addDouble("Area") { area }
+        visionTab.addDouble("Skew") { skew }
+        visionTab.addBoolean("Target Visible") { targetVisible }
+
+        // FIXME - remove if possible
+        // Create a Shuffleboard tab for the Limelight
+        val visionTab: ShuffleboardTab = Shuffleboard.getTab("Vision")
+    }
+
+    /**
+     * Gets the offset of the cross-hair to the target on the x-axis.
+     * Right is positive
+     *
+     * @return the x offset
+     */
+    val xOffset: Double
+        get() = tx.getDouble(0.0)
+
+    /**
+     * Gets the offset of the cross-hair to the target on the y-axis.
+     * Up is positive
+     *
+     * @return the y offset
+     */
+    val yOffset: Double
+        get() = ty.getDouble(0.0)
+
+    // TODO 0.0-1.0 or 0.0-100.0 ??
+    /**
+     * Gets the percentage of the camera field of view that the target is visible
+     *
+     * @return how much of the screen can see the target
+     */
+    val area: Double
+        get() = ta.getDouble(0.0)
+
+    // FIXME: Potentially deprecated; what did it even do?
+    @Deprecated("Lack of documentation")
+    val skew: Double
+        get() = ts.getDouble(0.0)
+
+    /**
+     * Checks whether the target object is visible to the limelight camera
+     *
+     * @return `true` if the target object is visible to the limelight camera,
+     * `false` otherwise.
+     */
+    val targetVisible: Boolean
+        get() = tv.getDouble(0.0) == 1.0
+}
