07b25c3151b08066641dab1100336c8cba6ac591
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-01-07 08:16:34 -0500

    Dev into Limelight Helpers (#22)
    * i hate this tooo
    
    * KTLINT (#17)
    
    * ktLint Install
    
    * Formatted
    
    * Formatted
    
    * added precommit hook
    
    * Test
    
    * edited pre-commit
    
    * added pre-commit copier
    
    * added pre-commit copier to building and deploying
    
    * bro was exiting before formatting
    
    * Fixed the weird builds
    
    * Revert "Fixed the weird builds"
    
    This reverts commit 9dfe9ceb906c620ddda84f8c32731e97bd20aac0.
    
    * i hate this
    
    ---------
    
    Co-authored-by: Micah Rao <raoboys12@gmail.com>
    
    * Revert "Merge branch '14-create-limelight-helper-classes' into dev"
    
    This reverts commit 14e90cc1a0c787b0ab48417debc4dcfcc29d5d7a, reversing
    changes made to 4a8a829e2e4facc9a070fbc3ab643dee57382353.
    
    * Fixed up (I think) the ktlint build issues
    
    * Revert "Fixed up (I think) the ktlint build issues"
    
    This reverts commit caad49fe7a8d59a74145d3a0ee55b40f12aead0b.
    
    ---------
    
    Co-authored-by: Micah Rao <raoboys12@gmail.com>

--- src/main/java/frc/robot/commands/vision/TrackTargetCommand.kt
+++ src/main/java/frc/robot/commands/vision/TrackTargetCommand.kt
@@ -16,22 +16,17 @@ class TrackTargetCommand : CommandBase() {
     private var rotation: Double = 0.0
     private var translation: Translation2d = Translation2d(0.0, 0.0)
 
-
     init {
         // each subsystem used by the command must be passed into the addRequirements() method
         addRequirements(limelightSubsystem, drivebase)
         pidController = PIDController(0.1, 0.0, 0.0)
-
-
     }
 
     override fun initialize() {}
 
     override fun execute() {
         rotation = pidController.calculate(limelightSubsystem.xOffset, 0.0)
-
-
-
+        
         if((abs(limelightSubsystem.xOffset) < 2 && limelightSubsystem.area < 3.5) && limelightSubsystem.targetVisible) {
             translation = Translation2d(0.3, -SingletonXboxController.leftX)
         } else {
