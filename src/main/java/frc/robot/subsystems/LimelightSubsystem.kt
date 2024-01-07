ff8d8a7a4ad1fe8fb50b464642286dc7f01958c8
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-01-07 08:20:08 -0500

    Revert "Dev into Limelight Helpers (#22)"
    This reverts commit 07b25c3151b08066641dab1100336c8cba6ac591.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -1,75 +1,25 @@
 package frc.robot.subsystems
 
 import edu.wpi.first.networktables.NetworkTableInstance
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import lib.Limelight
 
 object LimelightSubsystem : SubsystemBase() {
 
+    private val limelight : Limelight = Limelight(NetworkTableInstance.getDefault().getTable("limelight"))
 
+    val xOffset: Double
+        get() = limelight.xOffset
 
-    // NetworkTable object for getting data from the Limelight
-    private var table: NetworkTable
-    private var visionTab: ShuffleboardTab
+    val yOffset: Double
+        get() = limelight.yOffset
 
-    // Variables for storing data from the Limelight
-    private var xOffset: Double = 0.0
-    private var yOffset: Double = 0.0
-    private var area: Double = 0.0
-    private var skew: Double = 0.0
-    private var targetVisible: Boolean = false
+    val area: Double
+        get() = limelight.area
 
-    init {
-        // Get the default NetworkTable for the Limelight
-        table = NetworkTableInstance.getDefault().getTable("limelight")
+    val skew: Double
+        get() = limelight.skew
 
-        // Get the NetworkTableEntry objects for the Limelight
-        tx = table.getEntry("tx")
-        ty = table.getEntry("ty")
-        ta = table.getEntry("ta")
-        tv = table.getEntry("tv")
-        ts = table.getEntry("ts")
-
-        visionTab = Shuffleboard.getTab("Vision")
-
-        visionTab.addDouble("X Offset") { xOffset }
-        visionTab.addDouble("Y Offset") { yOffset }
-        visionTab.addDouble("Area") { area }
-        visionTab.addDouble("Skew") { skew }
-        visionTab.addBoolean("Target Visible") { targetVisible }
-
-        // Create a Shuffleboard tab for the Limelight
-        val visionTab: ShuffleboardTab = Shuffleboard.getTab("Vision")
-    }
-
-    override fun periodic() {
-        // Update the data with the latest values from the Limelight
-        xOffset = tx.getDouble(0.0)
-        yOffset = ty.getDouble(0.0)
-        area = ta.getDouble(0.0)
-        skew = ts.getDouble(0.0)
-        targetVisible = tv.getDouble(0.0) == 1.0
-    }
-
-    // Getters for the data from the Limelight
-    fun getXOffset(): Double {
-        return xOffset
-    }
-
-    fun getYOffset(): Double {
-        return yOffset
-    }
-
-    fun getArea(): Double {
-        return area
-    }
-
-    fun getSkew(): Double {
-        return skew
-    }
-
-    fun isTargetVisible(): Boolean {
-        return targetVisible
-    }
+    val targetVisible: Boolean
+        get() = limelight.targetVisible
 }
