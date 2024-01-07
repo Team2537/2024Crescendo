49b04b4c3dd340264428fba77b3e07efcb35897a
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-01-07 08:24:15 -0500

    Refactored lib folder

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -1,12 +1,11 @@
 package frc.robot.subsystems
 
 import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import lib.Limelight
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import lib.vision.Limelight
 
 object LimelightSubsystem : SubsystemBase() {
-
-    private val limelight : Limelight = Limelight(NetworkTableInstance.getDefault().getTable("limelight"))
+    private val limelight: Limelight = Limelight(NetworkTableInstance.getDefault().getTable("limelight"))
 
     val xOffset: Double
         get() = limelight.xOffset
