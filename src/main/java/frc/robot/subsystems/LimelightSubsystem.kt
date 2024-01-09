88bd6fccc21aeb7441a8a2e0a11b8815c8802fa7
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-01-09 12:55:04 -0500

    idk why it keeps breaking this

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -1,7 +1,5 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.networktables.NetworkTable
-import edu.wpi.first.networktables.NetworkTableEntry
 import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import lib.vision.Limelight
@@ -9,28 +7,18 @@ import lib.vision.Limelight
 object LimelightSubsystem : SubsystemBase() {
     private val limelight: Limelight = Limelight(NetworkTableInstance.getDefault().getTable("limelight"))
 
+    val xOffset: Double
+        get() = limelight.xOffset
 
-    // Variables for storing data from the Limelight
-    private var xOffset: Double = 0.0
-    private var yOffset: Double = 0.0
-    private var area: Double = 0.0
-    private var skew: Double = 0.0
-    private var targetVisible: Boolean = false
+    val yOffset: Double
+        get() = limelight.yOffset
 
-    init {
-        // Get the default NetworkTable for the Limelight
-        table = NetworkTableInstance.getDefault().getTable("limelight")
+    val area: Double
+        get() = limelight.area
 
-        // Get the NetworkTableEntry objects for the Limelight
-        tx = table.getEntry("tx")
-        ty = table.getEntry("ty")
-        ta = table.getEntry("ta")
-        tv = table.getEntry("tv")
-        ts = table.getEntry("ts")
-
-        visionTab = Shuffleboard.getTab("Vision")
+    val skew: Double
+        get() = limelight.skew
 
     val targetVisible: Boolean
         get() = limelight.targetVisible
-
 }
