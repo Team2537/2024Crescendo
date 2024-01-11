e15cbe801d1f9c8a9e0b88c188e083c73ae0a3a6
jbvargh <joshvarghese47@gmail.com>
2024-01-11 18:32:02 -0500

    Added variables for various inputs for abstract controllers

--- src/main/java/lib/controllers/Gamepad.kt
+++ src/main/java/lib/controllers/Gamepad.kt
@@ -1,10 +1,24 @@
 package lib.controllers
 
 interface Gamepad {
-    val leftX: Double
-    val leftY: Double
-    val rightX: Double
-    val rightY: Double
-    val leftTrigger: Double
-    val rightTrigger: Double
+    val leftXAxis: Double
+    val leftYAxis: Double
+    val leftTriggerAxis: Double
+    val rightTriggerAxis: Double
+    val rightXAxis: Double
+    val rightYAxis: Double
+    val aButton: Boolean
+    val bButton: Boolean
+    val xButton: Boolean
+    val yButton: Boolean
+    val leftJoystickButton: Boolean
+    val rightJoystickButton: Boolean
+    val leftShoulderButton: Boolean
+    val rightShoulderButton: Boolean
+    val dPadUp: Boolean
+    val dPadDown: Boolean
+    val dPadLeft: Boolean
+    val dPadRight: Boolean
+    val startButton: Boolean
+    val selectButton: Boolean
 }
