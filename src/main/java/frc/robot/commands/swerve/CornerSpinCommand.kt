beef1f4b031a5f8e49914c80332f49f6d3f55aad
Micah Rao <raoboys12@gmail.com>
2024-01-13 10:49:35 -0500

    Change DriverProfile.kt to fit standard present in Autos.kt
    Some Refactoring Done

--- src/main/java/frc/robot/commands/swerve/CornerSpinCommand.kt
+++ src/main/java/frc/robot/commands/swerve/CornerSpinCommand.kt
@@ -1,6 +1,6 @@
 package frc.robot.commands.swerve
 
-import SwerveSubsystem
+import frc.robot.subsystems.SwerveSubsystem
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.util.SingletonXboxController
