0ebd55e1f7da4a8600f7139d3a89a2faff5d43b0
Micah Rao <raoboys12@gmail.com>
2024-01-25 18:28:02 -0500

    Launcher Subsystem Boilerplate

---
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
@@ -0,0 +1,10 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+object LauncherSubsystem : SubsystemBase() {
+    init {
+
+    }
+    override fun periodic() {}
+}
