06ee68eff7265b34aa24c8b75d391d2b7c79eb14
Dione Laptop <social@team2537.com>
2024-01-25 19:28:40 -0500

    Added Angle control to launcher

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
@@ -1,10 +1,40 @@
 package frc.robot.subsystems
 
+import com.revrobotics.CANSparkFlex
+import com.revrobotics.CANSparkLowLevel
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants
 
 object LauncherSubsystem : SubsystemBase() {
-    init {
+    val angleMotor = CANSparkFlex(Constants.LauncherConstants.ANGLE_MOTOR, CANSparkLowLevel.MotorType.kBrushless)
 
+
+    private val kP = Constants.LauncherConstants.ANGLE_KP
+    private val kI = Constants.LauncherConstants.ANGLE_KI
+    private val kD = Constants.LauncherConstants.ANGLE_KD
+    private val pidController = PIDController(kP, kI, kD)
+
+    init {
+        val driveTab: ShuffleboardTab = Shuffleboard.getTab("Launcher Subsystem")
+        val pid = angleMotor.pidController
+        pid.p=kP
+        pid.i=kI
+        pid.d=kD
+
+        driveTab.addNumber("Motor Velocity")
+        {
+            angleMotor.encoder.velocity
+        }
+
+        fun set(rA: Double){
+            angleMotor.set(rA)
+        }
+        fun setAngle(pos: Double){
+            pidController.setpoint = pos
+        }
     }
     override fun periodic() {}
 }
