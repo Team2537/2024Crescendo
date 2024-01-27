4a0882b8bb4645d8de639d93fd50dc982c35b10d
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2024-01-27 10:49:42 -0500

    Add zoneTrigger util functions

--- src/main/java/lib/Util.kt
+++ src/main/java/lib/Util.kt
@@ -1,8 +1,12 @@
 package lib
 
+import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.subsystems.SwerveSubsystem
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
+import lib.zones.Zone
+import lib.zones.Zones
 import kotlin.math.absoluteValue
 import kotlin.math.pow
 
@@ -34,4 +38,24 @@ fun Boolean.toTrigger(): Trigger {
 
 inline fun <reified T> Json.encodeToString(data: T): String {
     return encodeToString(serializersModule.serializer(), data)
+}
+
+/**
+ * Returns a trigger that is `true` while the position is inside the zone, and
+ * `false` otherwise.
+ *
+ * @return A trigger that checks against a specific zone
+ */
+inline fun zoneTrigger(tag: String, crossinline position: () -> Pose2d = { SwerveSubsystem.getPose() }): Trigger {
+    return zoneTrigger(Zones[tag], position)
+}
+
+/**
+ * Returns a trigger that is `true` while the position is inside the zone, and
+ * `false` otherwise.
+ *
+ * @return A trigger that checks against a specific zone
+ */
+inline fun zoneTrigger(zone: Zone, crossinline position: () -> Pose2d = { SwerveSubsystem.getPose() }): Trigger {
+    return Trigger { Zones[position.invoke()] == zone }
 }
