8d5d500ec69e6410246d5babbadecac175bafcbc
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2024-01-27 16:05:16 -0500

    Add pitch get function

--- src/main/java/lib/Util.kt
+++ src/main/java/lib/Util.kt
@@ -1,6 +1,10 @@
 package lib
 
 import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.units.Angle
+import edu.wpi.first.units.Distance
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.subsystems.SwerveSubsystem
 import kotlinx.serialization.json.Json
@@ -8,6 +12,7 @@ import kotlinx.serialization.serializer
 import lib.zones.Zone
 import lib.zones.Zones
 import kotlin.math.absoluteValue
+import kotlin.math.atan
 import kotlin.math.pow
 
 /**
@@ -58,4 +63,41 @@ inline fun zoneTrigger(tag: String, crossinline position: () -> Pose2d = { Swerv
  */
 inline fun zoneTrigger(zone: Zone, crossinline position: () -> Pose2d = { SwerveSubsystem.getPose() }): Trigger {
     return Trigger { Zones[position.invoke()] == zone }
+}
+
+private fun getAngleToShootRaw(
+    targetHeight: Double,
+    targetDistance: Double,
+    launcherHeight: Double,
+    epsilon: Double = 0.001
+): Double {
+    return atan((targetHeight - launcherHeight) / targetDistance) + epsilon * 9.8 * targetDistance
+}
+
+/**
+ * Gets the pitch angle required (approximately) to hit a target at a given distance.
+ *
+ * This function assumes that the projectile will travel in a straight line, while
+ * giving a minor positive offset based on the distance to account for gravity.
+ *
+ * @param targetHeight The height of the target
+ * @param targetDistance The distance from the launcher to the target
+ * @param launcherHeight The height of the launcher
+ * @param epsilon How much to scale the distance/gravity offset
+ *
+ * @return An angle of elevation to aim at relative to the horizontal
+ */
+fun getPitchTowardsTarget(
+    targetHeight: Measure<Distance>,
+    targetDistance: Measure<Distance>,
+    launcherHeight: Measure<Distance>,
+    epsilon: Double = 0.001
+): Measure<Angle> {
+    return Units.Radians.of(
+        getAngleToShootRaw(
+            targetHeight.`in`(Units.Meters),
+            targetDistance.`in`(Units.Meters),
+            launcherHeight.`in`(Units.Meters),
+            epsilon)
+    )
 }
