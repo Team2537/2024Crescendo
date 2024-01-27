6b6353c981453b15b92234e793d48231c3966192
Micah Rao <raoboys12@gmail.com>
2024-01-27 05:37:00 -0500

    Refactoring/Docs

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.kt
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.kt
@@ -1,12 +1,18 @@
 package frc.robot.subsystems
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType
 import com.revrobotics.CANSparkMax
 import frc.robot.Constants.IntakeConstants
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import lib.setVelocity
+import edu.wpi.first.wpilibj2.command.SubsystemBase
 
+/**
+ * The Intake subsystem contains methods and objects for controlling the under-the-bumper intake system,
+ * as well as the transfer conveyor(?) to the launcher.
+ */
 object IntakeSubsystem : SubsystemBase() {
+    /**
+     * [REV Neo 550](https://www.revrobotics.com/rev-21-1651/) that controls the under-bumper intake.
+     */
     val intakeMotor : CANSparkMax = CANSparkMax(IntakeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless)
 
     init {
