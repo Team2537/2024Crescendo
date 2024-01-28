afd08bc7e823705d1ed5ffb9eb43e2e985063c0b
Micah Rao <raoboys12@gmail.com>
2024-01-28 13:55:57 -0500

    Added Logging

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.kt
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.kt
@@ -6,8 +6,11 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.math.kinematics.SwerveModuleState
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.math.util.Units
+import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.networktables.StructArrayPublisher
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 import edu.wpi.first.wpilibj2.command.Command
@@ -30,6 +33,8 @@ object SwerveSubsystem : SubsystemBase() {
     /** @suppress */
     var maximumSpeed: Double = Units.feetToMeters(12.0)
     var tab: ShuffleboardTab = Shuffleboard.getTab("Testing")
+    var swerveStates: StructArrayPublisher<SwerveModuleState> = NetworkTableInstance.getDefault().
+        getStructArrayTopic("SwerveStates/swerveStates", SwerveModuleState.struct).publish()
 
     init {
         try {
@@ -228,5 +233,9 @@ object SwerveSubsystem : SubsystemBase() {
 
     fun addVisionMeasurement(measurement: VisionMeasurement) {
         swerveDrive.addVisionMeasurement(measurement.position.toPose2d(), measurement.timestamp)
+    }
+
+    override fun periodic() {
+        swerveStates.set(swerveDrive.states)
     }
 }
