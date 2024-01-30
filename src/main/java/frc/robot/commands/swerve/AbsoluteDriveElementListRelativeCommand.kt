cc681d28031388f58e4e3cd97dafba59f16d046e
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2024-01-30 18:25:16 -0500

    Refactor getting rotation towards target

--- src/main/java/frc/robot/commands/swerve/AbsoluteDriveElementListRelativeCommand.kt
+++ src/main/java/frc/robot/commands/swerve/AbsoluteDriveElementListRelativeCommand.kt
@@ -6,7 +6,8 @@ import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.SwerveSubsystem
-import lib.getAngleTo
+import lib.angle
+import lib.getRotationTo
 import swervelib.SwerveController
 import java.util.function.DoubleSupplier
 
@@ -36,7 +37,7 @@ class AbsoluteDriveElementListRelativeCommand(
     override fun execute() {
         val pose = swerveSubsystem.getPose()
         val closestElement = pose.nearest(elements)
-        val angle = pose.getAngleTo(closestElement)
+        val angle = pose.getRotationTo(closestElement).angle
         val rotation = anglePID.calculate(swerveSubsystem.getHeading().degrees, angle.`in`(Units.Degrees)) * controller.config.maxAngularVelocity
 
         val forwardVelocity = vForward.asDouble * swerveSubsystem.maximumSpeed
