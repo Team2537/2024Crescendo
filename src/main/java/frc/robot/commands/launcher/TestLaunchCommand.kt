7220bc8303444447b5913b18b341f13bff55ffcd
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2024-02-03 16:49:04 -0500

    Add timer to TestLaunchCommand

--- src/main/java/frc/robot/commands/launcher/TestLaunchCommand.kt
+++ src/main/java/frc/robot/commands/launcher/TestLaunchCommand.kt
@@ -1,31 +1,41 @@
 package frc.robot.commands.launcher
 
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.LauncherSubsystem
 
 class TestLaunchCommand : Command() {
     private val launcherSubsystem = LauncherSubsystem
 
+    private val timer: Timer
 
     init {
         // each subsystem used by the command must be passed into the addRequirements() method
         addRequirements(launcherSubsystem)
+
+        timer = Timer()
     }
 
     override fun initialize() {
         launcherSubsystem.setLaunchVelocity(6700.0)
+        timer.restart()
     }
 
     override fun execute() {
         if(launcherSubsystem.leftLauncherMotor.encoder.velocity > 6600.0){
             launcherSubsystem.outtake()
         }
+
+        if(launcherSubsystem.piecePresent){
+            timer.reset()
+        }
     }
 
     override fun isFinished(): Boolean {
-        // TODO: Make this return true when this Command no longer needs to run execute()
-        return false
+        return timer.hasElapsed(0.3)
     }
 
-    override fun end(interrupted: Boolean) {}
+    override fun end(interrupted: Boolean) {
+        timer.stop()
+    }
 }
