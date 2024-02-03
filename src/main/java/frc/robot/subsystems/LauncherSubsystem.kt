c37f42ee7a540fc840cf28f5ec5b0e83e43927e7
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-02-03 15:59:45 -0500

    Firing the launcher (#57)
    * Finished the LauncherSubsystem with both the CANSparkFlex Launcher Motors with device ID 1 and 2.
    
    * Refactored Launcher Code
    
    * More Refactoring
    
    * Combining Launcher Control (#52)
    
    * Created Launcher Constants
    
    * Added Angle control to launcher
    
    * Created Launcher Constants
    
    * Added Angle control to launcher
    
    * Refactoring Aiming
    
    ---------
    
    Co-authored-by: Dione Laptop <social@team2537.com>
    Co-authored-by: Micah Rao <raoboys12@gmail.com>
    
    * Updated constants to add storeMotor
    
    * Add storeMotor to launcher, plus refactoring
    
    ---------
    
    Co-authored-by: UserInterfacing <destruction014@gmail.com>
    Co-authored-by: Micah Rao <raoboys12@gmail.com>
    Co-authored-by: Dione Laptop <social@team2537.com>

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
@@ -1,186 +1,69 @@
-import com.revrobotics.*
-import edu.wpi.first.wpilibj.DigitalInput
-import edu.wpi.first.wpilibj.Timer
-import edu.wpi.first.wpilibj2.command.SubsystemBase
-import edu.wpi.first.wpilibj2.command.button.Trigger
-import frc.robot.Constants
-import lib.near
-import lib.zoneTrigger
+package frc.robot.subsystems
+
+import com.revrobotics.CANSparkBase
+import com.revrobotics.CANSparkFlex
+import com.revrobotics.CANSparkLowLevel
+import com.revrobotics.CANSparkMax
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LauncherConstants
 
 object LauncherSubsystem : SubsystemBase() {
+    val angleMotor = CANSparkMax(LauncherConstants.ANGLE_MOTOR_PORT, CANSparkLowLevel.MotorType.kBrushless)
 
-    private val leftLauncher: CANSparkFlex = CANSparkFlex(Constants.LauncherConstants.LEFT_LAUNCHER_PORT,
-        CANSparkLowLevel.MotorType.kBrushless)
+    val storeMotor: CANSparkMax = CANSparkMax(LauncherConstants.STORE_MOTOR_PORT, CANSparkLowLevel.MotorType.kBrushless)
+  
+    val leftLauncherMotor : CANSparkFlex = CANSparkFlex(LauncherConstants.LEFT_LAUNCHER_PORT, CANSparkLowLevel.MotorType.kBrushless)
+    val rightLauncherMotor : CANSparkFlex = CANSparkFlex(LauncherConstants.RIGHT_LAUNCHER_PORT, CANSparkLowLevel.MotorType.kBrushless)
 
-    private val rightLauncher: CANSparkFlex = CANSparkFlex(Constants.LauncherConstants.RIGHT_LAUNCHER_PORT,
-        CANSparkLowLevel.MotorType.kBrushless)
 
-    private val rollerMotor: CANSparkMax = CANSparkMax(Constants.LauncherConstants.ROLLER_MOTOR_PORT,
-        CANSparkLowLevel.MotorType.kBrushless)
+    val launchPID = leftLauncherMotor.pidController
+    val storePID = storeMotor.pidController
+    val anglePID = angleMotor.pidController
 
-    private val rollerPID: SparkPIDController = rollerMotor.pidController
-
-    private val noteDetector: Trigger
-
-    private val noteTimer: Timer = Timer()
-
-    var fire: Boolean = false
-
-    enum class State {
-        STORED,
-        EMPTY,
-        PRIMED,
-        AT_SPEED,
-        FIRING
-    }
-
-    var state: State = State.EMPTY
-        get() = this.state
-
-    var storePosition: Double = getRollerPosition()
 
     init {
-        configureMotors()
+        val driveTab: ShuffleboardTab = Shuffleboard.getTab("Launcher Subsystem")
 
-        val irSensor: DigitalInput = DigitalInput(Constants.LauncherConstants.INFRARED_SENSOR)
-        noteDetector = Trigger() { !irSensor.get() }
-        noteTimer.start()
+
+        anglePID.p = LauncherConstants.ANGLE_KP
+        anglePID.i = LauncherConstants.ANGLE_KI
+        anglePID.d = LauncherConstants.ANGLE_KD
+      
+        launchPID.p = LauncherConstants.LAUNCHER_P
+        launchPID.i = LauncherConstants.LAUNCHER_I
+
+        rightLauncherMotor.follow(leftLauncherMotor, true)
+
+
+        driveTab.addNumber("Motor1 Velocity") {
+            leftLauncherMotor.encoder.velocity
+            rightLauncherMotor.encoder.velocity
+        }
+
+        driveTab.addNumber("Motor Velocity") {
+            angleMotor.encoder.velocity
+        }
+
     }
 
-
-    private fun configureMotors(){
-        leftLauncher.restoreFactoryDefaults()
-        rightLauncher.restoreFactoryDefaults()
-        rollerMotor.restoreFactoryDefaults()
-
-        rightLauncher.follow(leftLauncher, true)
-
-        configureEncoders()
-        configurePID()
-
-        leftLauncher.setIdleMode(CANSparkBase.IdleMode.kBrake)
-        rightLauncher.setIdleMode(CANSparkBase.IdleMode.kBrake)
-        rollerMotor.setIdleMode(CANSparkBase.IdleMode.kBrake)
+    fun setAngle(angle: Double) {
+        anglePID.setReference(angle, CANSparkBase.ControlType.kPosition)
+    }
+    
+    fun setRawLaunchSpeed(rA: Double, rB: Double) {
+        leftLauncherMotor.set(rA)
     }
 
-    private fun configurePID() {
-        rollerPID.p = Constants.LauncherConstants.ROLLER_P
-        rollerPID.i = Constants.LauncherConstants.ROLLER_I
-        rollerPID.d = 0.0
-
-        rollerPID.setSmartMotionAccelStrategy(SparkPIDController.AccelStrategy.kTrapezoidal, 0)
-        rollerPID.setSmartMotionMaxVelocity(240.0, 0)
-        rollerPID.setSmartMotionMinOutputVelocity(0.0, 0)
-        rollerPID.setSmartMotionAllowedClosedLoopError(0.05, 0)
-        rollerPID.setSmartMotionMaxAccel(1000.0, 0)
+    fun setLaunchVelocity(velocity: Double){
+        launchPID.setReference(velocity, CANSparkBase.ControlType.kVelocity)
     }
-
-    private fun configureEncoders() {
-        leftLauncher.encoder.positionConversionFactor = 1.0
-        leftLauncher.encoder.velocityConversionFactor = 1.0
-
-        rightLauncher.encoder.positionConversionFactor = 1.0
-        rightLauncher.encoder.velocityConversionFactor = 1.0
-
-        rollerMotor.encoder.positionConversionFactor = 1.0
-        rollerMotor.encoder.velocityConversionFactor = 1.0
-    }
-
-    fun setLauncherSpeed(speed: Double){
-        leftLauncher.set(speed)
-    }
-
-    fun getLauncherVelocity(): Double {
-        return leftLauncher.encoder.velocity
-    }
-
-    fun setRollerSpeed(speed: Double) {
-        rollerMotor.set(speed)
-    }
-
-    fun stop() {
-        setRollerMode(CANSparkBase.IdleMode.kBrake)
-
-        leftLauncher.set(0.0)
-        rollerMotor.set(0.0)
-    }
-
-    fun setRollerMode(idleMode: CANSparkBase.IdleMode){
-        rollerMotor.setIdleMode(idleMode)
-    }
-
+    
     fun intake(){
-        storePosition = getRollerPosition() - 1
-        println("Intake ran, setting to $storePosition")
-        rollerPID.setReference(storePosition, CANSparkBase.ControlType.kSmartMotion)
-    }
-
-    fun getRollerPosition(): Double {
-        return rollerMotor.encoder.position
-    }
-
-    fun inZone(): Boolean {
-        return zoneTrigger("primeZone").asBoolean
-    }
-
-    fun updateState(){
-        when(state){
-            State.EMPTY -> {
-                stop()
-                if(!noteDetector.asBoolean){
-                    state = State.STORED
-                }
-                rollerPID.p = 0.0001
-                rollerPID.i = 0.000001
-            }
-            State.STORED -> {
-                if(noteDetector.asBoolean){
-                    state = State.EMPTY
-                } else if (!noteDetector.asBoolean && storePosition.near(getRollerPosition(), 0.2) && inZone()){
-                    state = State.PRIMED
-                }
-                rollerPID.p = 0.0001
-                rollerPID.i = 0.000001 // TODO: Tune?
-            }
-            State.PRIMED -> {
-                if(getLauncherVelocity() > 6000 && leftLauncher.encoder.velocity > 6000){
-                    state = State.AT_SPEED
-                } else if (noteDetector.asBoolean) {
-                    state = State.EMPTY
-                }
-            }
-            State.AT_SPEED -> {
-                if(fire){
-                    fire = false
-                    state = State.FIRING
-                }
-            }
-            State.FIRING -> {
-                if(noteTimer.hasElapsed(0.5)){
-                    state = State.EMPTY
-                }
-            }
-        }
-    }
-
-    fun getState(): State {
-        return state
-    }
-
-    fun setState(state: State){
-        this.state = state
-    }
-
-    fun triggerFactory(state: State): Trigger {
-        return Trigger() { getState() == state}
-    }
-
-    override fun periodic() {
-        if(noteDetector.asBoolean){
-            noteTimer.reset()
-        }
+        
     }
 
 
-
+    override fun periodic() {}
 }
