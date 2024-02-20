da660d814fe646849598b0bb95b610970dd27936
Europa <falon.n.clark@gmail.com>
2024-02-20 15:12:01 -0500

    Replaced old code with skeleton code

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.kt
@@ -1,84 +1,84 @@
-package frc.robot.subsystems
-
 import com.revrobotics.*
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.Measure
-import edu.wpi.first.units.Units
-import edu.wpi.first.units.Velocity
 import edu.wpi.first.wpilibj.DigitalInput
-import edu.wpi.first.wpilibj.DutyCycleEncoder
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.Constants
-import frc.robot.Constants.LauncherConstants
 
 object LauncherSubsystem : SubsystemBase() {
 
-    val angleMotor = CANSparkMax(LauncherConstants.ANGLE_MOTOR_PORT, CANSparkLowLevel.MotorType.kBrushless)
-    val storeMotor: CANSparkMax = CANSparkMax(LauncherConstants.STORE_MOTOR_PORT, CANSparkLowLevel.MotorType.kBrushless)
-    val leftLauncherMotor : CANSparkFlex = CANSparkFlex(LauncherConstants.LEFT_LAUNCHER_PORT, CANSparkLowLevel.MotorType.kBrushless)
-    val rightLauncherMotor : CANSparkFlex = CANSparkFlex(LauncherConstants.RIGHT_LAUNCHER_PORT, CANSparkLowLevel.MotorType.kBrushless)
+    private val leftLauncher: CANSparkFlex = CANSparkFlex(Constants.LauncherConstants.LEFT_LAUNCHER_PORT,
+        CANSparkLowLevel.MotorType.kBrushless)
 
+    private val rightLauncher: CANSparkFlex = CANSparkFlex(Constants.LauncherConstants.RIGHT_LAUNCHER_PORT,
+        CANSparkLowLevel.MotorType.kBrushless)
 
-    val launchPID: SparkPIDController = leftLauncherMotor.pidController
-    val storePID: SparkPIDController = storeMotor.pidController
-    val anglePID: SparkPIDController = angleMotor.pidController
+    private val rollerMotor: CANSparkMax = CANSparkMax(Constants.LauncherConstants.ROLLER_MOTOR_PORT,
+        CANSparkLowLevel.MotorType.kBrushless)
 
-    val launchEncoder: RelativeEncoder = leftLauncherMotor.encoder
-    val storeEncoder: RelativeEncoder = storeMotor.encoder
+    private val rollerPID: SparkPIDController = rollerMotor.pidController
 
-    val relativeAngleEncoder: RelativeEncoder = angleMotor.encoder
-    val absoluteAngleEncoder: DutyCycleEncoder = DutyCycleEncoder(LauncherConstants.ABSOLUTE_ENCODER_PORT)
+    private val noteDetector: Trigger
 
-    val irPieceSensor = DigitalInput(Constants.LauncherConstants.IR_PIECE_SENSOR_ID)
+    private val noteTimer: Timer = Timer()
 
+    enum class State {
+        STORED,
+        EMPTY,
+        PRIMED,
+        AT_SPEED,
+        FIRING
+    }
+
+    var state: State = State.EMPTY
+        get() = this.state
 
     init {
-        val driveTab: ShuffleboardTab = Shuffleboard.getTab("Launcher Subsystem")
+        configureMotors()
 
-
-        anglePID.p = LauncherConstants.ANGLE_KP
-        anglePID.i = LauncherConstants.ANGLE_KI
-        anglePID.d = LauncherConstants.ANGLE_KD
-      
-        launchPID.p = LauncherConstants.LAUNCHER_P
-        launchPID.i = LauncherConstants.LAUNCHER_I
-
-        rightLauncherMotor.follow(leftLauncherMotor, true)
-
-
-        driveTab.addNumber("Motor1 Velocity") {
-            leftLauncherMotor.encoder.velocity
-            rightLauncherMotor.encoder.velocity
-        }
-
-        driveTab.addNumber("Motor Velocity") {
-            angleMotor.encoder.velocity
-        }
-
+        val irSensor: DigitalInput = DigitalInput(Constants.LauncherConstants.INFRARED_SENSOR)
+        noteDetector = Trigger() { !irSensor.get() }
+        noteTimer.start()
     }
 
-    fun setAngle(angle: Measure<Angle>) {
-        anglePID.setReference(angle.`in`(Units.Rotations), CANSparkBase.ControlType.kPosition)
-    }
-    
-    fun setRawLaunchSpeed(rA: Double, rB: Double) {
-        leftLauncherMotor.set(rA)
+
+    private fun configureMotors(){
+        leftLauncher.restoreFactoryDefaults()
+        rightLauncher.restoreFactoryDefaults()
+        rollerMotor.restoreFactoryDefaults()
+
+        rightLauncher.follow(leftLauncher, true)
+
+        configureEncoders()
+        configurePID()
+
+        leftLauncher.setIdleMode(CANSparkBase.IdleMode.kBrake)
+        rightLauncher.setIdleMode(CANSparkBase.IdleMode.kBrake)
+        rollerMotor.setIdleMode(CANSparkBase.IdleMode.kBrake)
     }
 
-    fun setLaunchVelocity(velocity: Measure<Velocity<Angle>>) {
-        launchPID.setReference(velocity.`in`(Units.RPM), CANSparkBase.ControlType.kVelocity)
-    }
-    
-    fun intake() {
-        storePID.setReference(0.5, CANSparkBase.ControlType.kPosition)
-    }
-    val piecePresent: Boolean
-        get() = irPieceSensor.get()
-    fun outtake() {
-        storeMotor.set(0.5)
+    private fun configurePID() {
+        rollerPID.p = Constants.LauncherConstants.ROLLER_P
+        rollerPID.i = Constants.LauncherConstants.ROLLER_I
+        rollerPID.d = 0.0
+
+        rollerPID.setSmartMotionAccelStrategy(SparkPIDController.AccelStrategy.kTrapezoidal, 0)
+        rollerPID.setSmartMotionMaxVelocity(240.0, 0)
+        rollerPID.setSmartMotionMinOutputVelocity(0.0, 0)
+        rollerPID.setSmartMotionAllowedClosedLoopError(0.05, 0)
+        rollerPID.setSmartMotionMaxAccel(1000.0, 0)
     }
 
-    override fun periodic() {}
+    private fun configureEncoders() {
+        leftLauncher.encoder.positionConversionFactor = 1.0
+        leftLauncher.encoder.velocityConversionFactor = 1.0
+
+        rightLauncher.encoder.positionConversionFactor = 1.0
+        rightLauncher.encoder.velocityConversionFactor = 1.0
+
+        rollerMotor.encoder.positionConversionFactor = 1.0
+        rollerMotor.encoder.velocityConversionFactor = 1.0
+    }
+
+
 }
