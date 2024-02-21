e7f6000b91983b9faaf306c9903ed7142fb04c4e
Europa <falon.n.clark@gmail.com>
2024-02-21 16:36:35 -0500

    Refactored pivot for units library

--- src/main/java/frc/robot/commands/launcher/pivot/QuickPivotCommand.kt
+++ src/main/java/frc/robot/commands/launcher/pivot/QuickPivotCommand.kt
@@ -1,11 +1,15 @@
 package frc.robot.commands.launcher.pivot
 
+import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.PivotSubsystem
+import lib.math.units.Rotation
+import lib.math.units.into
+import lib.math.units.rotations
 
-class QuickPivotCommand(target: Double) : Command() {
+class QuickPivotCommand(target: Rotation) : Command() {
     private val pivotSubsystem = PivotSubsystem
-    private val target: Double
+    private val target: Rotation
     private var direction: Boolean = false
 
 
@@ -16,11 +20,11 @@ class QuickPivotCommand(target: Double) : Command() {
     }
 
     override fun initialize() {
-        direction = pivotSubsystem.getPosition() > target
+        direction = pivotSubsystem.getPosition() > target into Units.Rotations
     }
 
     override fun execute() {
-        if(pivotSubsystem.getPosition() > target){
+        if(pivotSubsystem.getPosition() > target into Units.Rotations){
             pivotSubsystem.rawMotorSpeed(-0.15)
         } else {
             pivotSubsystem.rawMotorSpeed(0.15)
@@ -30,9 +34,9 @@ class QuickPivotCommand(target: Double) : Command() {
     override fun isFinished(): Boolean {
         // TODO: Make this return true when this Command no longer needs to run execute()
         if(direction){
-            return pivotSubsystem.getPosition() < target
+            return pivotSubsystem.getPosition() < target into Units.Rotations
         } else {
-            return pivotSubsystem.getPosition() > target
+            return pivotSubsystem.getPosition() > target into Units.Rotations
         }
     }
 
