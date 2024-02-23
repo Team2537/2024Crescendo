6904fe7d8d08aa8327a2e47d1cc1b2044d313178
Something-Inconspicuous <webhead220@gmail.com>
2024-02-22 19:01:22 -0500

    Add property form of setGains

--- src/main/java/lib/pid/PIDUtils.kt
+++ src/main/java/lib/pid/PIDUtils.kt
@@ -14,3 +14,22 @@ fun SparkPIDController.setGains(gains: PIDGains){
     this.i = gains.kI
     this.d = gains.kD
 }
+
+var PIDController.gains: PIDGains
+    get() = PIDGains(p, i, d)
+    set(value){ setGains(value) }
+
+// Don't know why CANPIDController is deprecated while SparkPIDController, which implements it, is not,
+// but whatever.
+// Uncomment if we can/need to, but it doesn't seem to matter too much.
+//var CANPIDController.gains: PIDGains
+//    get() = PIDGains(p, i, d)
+//    set(value) {
+//        p = value.kP
+//        i = value.kI
+//        d = value.kD
+//    }
+
+var SparkPIDController.gains: PIDGains
+    get() = PIDGains(p, i, d)
+    set(value) { setGains(value) }
