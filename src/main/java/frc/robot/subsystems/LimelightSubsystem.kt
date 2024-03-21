254a15ec5f17ef8a5f712ba71f95dfe50d7adde2
Micah Rao <micah.s.rao@gmail.com>
2024-03-21 09:09:43 -0400

    Add Limelight distance estimate

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.kt
@@ -1,19 +1,12 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.math.geometry.Pose3d
-import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.units.Angle
-import edu.wpi.first.units.Measure
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import lib.vision.Limelight
-import lib.vision.LimelightHelpers
-import lib.vision.VisionMeasurement
 
 /**
  * The subsystem that controls the limelight.
  */
 object LimelightSubsystem : SubsystemBase() {
-
-    val odometryLimelight: Limelight = Limelight("limelight-odom") // FIXME
-    val pieceLimelight: Limelight = Limelight("limelight-TODO") // FIXME
+    val odometryLimelight: Limelight = Limelight("limelight-odom")
+    val intakeLimelight: Limelight = Limelight("limelight-intake")
 }
--- src/main/java/lib/vision/Limelight.kt
+++ src/main/java/lib/vision/Limelight.kt
@@ -1,5 +1,11 @@
 package lib.vision
 
+import edu.wpi.first.math.geometry.Translation3d
+import edu.wpi.first.units.Units.Radians
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import lib.math.units.degrees
+import lib.math.units.into
+import lib.math.units.pitch
 import lib.vision.LimelightHelpers.LimelightResults
 import lib.vision.LimelightHelpers.PoseEstimate
 
@@ -27,6 +33,12 @@ class Limelight(private val hostname: String) {
         get() = getTA()
 
     /**
+     * Distance from the limelight to the target
+     */
+    val td: Double
+        get() = getTD()
+
+    /**
      * Whether a target is visible
      */
     val tv: Boolean
@@ -37,6 +49,14 @@ class Limelight(private val hostname: String) {
 
     init {
         LimelightHelpers.setLEDMode_ForceOn(hostname)
+
+        val tab = Shuffleboard.getTab("Vision")
+
+        tab.addNumber("$hostname TX") { tx }
+        tab.addNumber("$hostname TY") { ty }
+        tab.addNumber("$hostname TA") { ta }
+        tab.addNumber("$hostname TD") { td }
+        tab.addBoolean("$hostname TV") { tv }
     }
 
     private fun getResults(): LimelightResults {
@@ -57,6 +77,13 @@ class Limelight(private val hostname: String) {
 
     private fun getTV(): Boolean {
         return LimelightHelpers.getTV(hostname)
+    }
+
+    private fun getTD(): Double {
+        val targetPose = LimelightHelpers.getTargetPose3d_RobotSpace(hostname)
+
+        // Target distance from origin (should be robot location)
+        return targetPose.translation.getDistance(Translation3d())
     }
 
     private fun getPoseEstimate(): PoseEstimate {
