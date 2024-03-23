99cc7fb95a2852bace493d50fcfece7566e88829
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-03-22 21:45:00 -0400

    Goodnight Folks

--- src/main/java/frc/robot/Robot.kt
+++ src/main/java/frc/robot/Robot.kt
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import frc.robot.commands.Autos
+import frc.robot.subsystems.PivotSubsystem
 import frc.robot.subsystems.SwerveSubsystem
 
 /**
@@ -72,6 +73,7 @@ object Robot : TimedRobot() {
         // This makes sure that the autonomous stops running when teleop starts running. If you want the
         // autonomous to continue until interrupted by another command, remove this line or comment it out.
         Autos.selectedAutonomousCommand.cancel()
+        PivotSubsystem.stop()
     }
 
     /** This method is called periodically during operator control.  */
