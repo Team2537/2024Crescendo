554bc138f82e4f57e304712f9ca9ec037eb9722f
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-06-14 14:48:11 -0400

    Better motion profile pivoting

---
+++ src/main/java/frc/robot/commands/pivot/AutoPivotCommand.kt
@@ -0,0 +1,51 @@
+package frc.robot.commands.pivot
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile
+import edu.wpi.first.units.Units
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.subsystems.PivotSubsystem
+import lib.math.units.inRPS
+import lib.math.units.inRadians
+import lib.math.units.into
+
+class AutoPivotCommand(
+    private val subsystem: PivotSubsystem,
+    private val setpoint: Double
+) : Command() {
+    private lateinit var startState: TrapezoidProfile.State
+    private lateinit var endState: TrapezoidProfile.State
+    private lateinit var targetState: TrapezoidProfile.State
+    private lateinit var profile: TrapezoidProfile
+    private var timer: Timer = Timer()
+
+    init {
+        addRequirements(subsystem)
+    }
+
+    override fun initialize() {
+        startState = TrapezoidProfile.State(
+            subsystem.getAngle().into(Units.Radians),
+            subsystem.getVelocity().into(Units.RadiansPerSecond)
+        )
+
+        endState = TrapezoidProfile.State(
+            setpoint,
+            0.0
+        )
+
+        timer.restart()
+    }
+
+    override fun execute() {
+        val elapsed = timer.get()
+        targetState = if(profile.isFinished(elapsed)){
+            endState
+        } else {
+            profile.calculate(elapsed, startState, endState)
+        }
+
+        val feedforward = subsystem.calculateFeedforward(subsystem.getAngle(), targetState.velocity.inRPS)
+        subsystem.applyPID(targetState.position.inRadians, feedforward)
+    }
+}
--- src/main/java/frc/robot/subsystems/PivotSubsystem.kt
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.kt
@@ -29,7 +29,7 @@ class PivotSubsystem : SubsystemBase() {
         .getLayout("Pivot", BuiltInLayouts.kList)
 
     private val angle: MutableMeasure<Angle> = MutableMeasure.zero(Units.Radians)
-    private var setpoint: Double = 0.0 // Radians, no units because its an underlying value so whatever
+    private val velocity: MutableMeasure<Velocity<Angle>> = MutableMeasure.zero(Units.RadiansPerSecond)
 
     /**
      * The feedforward controller for the pivot
@@ -42,14 +42,8 @@ class PivotSubsystem : SubsystemBase() {
         PivotConstants.kA,
     )
 
-    private val constraints: TrapezoidProfile.Constraints = TrapezoidProfile.Constraints(1.0, 0.5)
-    private lateinit var profile: TrapezoidProfile
-    private val timer: Timer = Timer()
+    val constraints: TrapezoidProfile.Constraints = TrapezoidProfile.Constraints(1.0, 0.5)
 
-    private lateinit var startState: TrapezoidProfile.State
-    private lateinit var endState: TrapezoidProfile.State
-
-    private lateinit var targetState: TrapezoidProfile.State
 
     init {
         pivotMotor.restoreFactoryDefaults()
@@ -72,59 +66,36 @@ class PivotSubsystem : SubsystemBase() {
         layout.addDouble("Output") { pivotMotor.appliedOutput * pivotMotor.busVoltage }
         layout.addDouble("IAccum") { pivotPIDController.iAccum }
 
-        timer.start()
-        updateProfile()
     }
 
 
     val homed: Boolean
         get() = homingSensor.get()
 
-    fun updateProfile() {
-        startState = TrapezoidProfile.State(pivotEncoder.position, pivotEncoder.velocity)
-        endState = TrapezoidProfile.State(setpoint, 0.0)
-        profile = TrapezoidProfile(constraints)
-        timer.reset()
-    }
 
     fun getAngle(): MutableMeasure<Angle> = angle.mut_replace(pivotEncoder.position.inRadians)
+    fun getVelocity(): MutableMeasure<Velocity<Angle>> = velocity.mut_replace(pivotEncoder.velocity.inRPS)
 
 
     private fun setVoltage(voltage: Measure<Voltage>) {
         pivotMotor.setVoltage(voltage.into(Units.Volts))
     }
 
-    fun setTargetPosition(position: Measure<Angle>) {
-        if(setpoint != position.into(Units.Radians)) {
-            setpoint = position.into(Units.Radians)
-            updateProfile()
-        }
+
+    fun calculateFeedforward(
+        angle: Measure<Angle>,
+        velocity: Measure<Velocity<Angle>>
+    ): Measure<Voltage> {
+        return feedforward.calculate(angle.into(Units.Radians), velocity.into(Units.RadiansPerSecond)).inVolts
     }
 
-    fun runAutomatic(){
-        val elapsedTime = timer.get()
-        targetState = if(profile.isFinished(elapsedTime)){
-            endState
-        } else {
-            profile.calculate(elapsedTime, startState, endState)
-        }
-
-        val feedforwardVoltage = feedforward.calculate(pivotEncoder.position, targetState.velocity)
-
+    fun applyPID(angle: Measure<Angle>, feedforward: Measure<Voltage>){
         pivotPIDController.setReference(
-            targetState.position,
+            angle.into(Units.Radians),
             CANSparkBase.ControlType.kPosition,
             0,
-            feedforwardVoltage,
+            feedforward.into(Units.Volts),
             SparkPIDController.ArbFFUnits.kVoltage
         )
     }
-
-
-//    fun calculateFeedforward(
-//        angle: Measure<Angle>,
-//        velocity: Measure<Velocity<Angle>>
-//    ): Measure<Voltage> {
-//        return feedforward.calculate(angle.into(Units.Radians), velocity.into(Units.RadiansPerSecond)).inVolts
-//    }
 }
