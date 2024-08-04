cf7cc893007a675d54dd06e5a232b819b68346b9
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-08-04 12:27:35 -0400

    it works!

--- src/main/java/lib/auto/ChoreoAuto.kt
+++ src/main/java/lib/auto/ChoreoAuto.kt
@@ -2,16 +2,26 @@ package lib.auto
 
 import com.choreo.lib.Choreo
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import frc.robot.subsystems.SwerveSubsystem
 import java.util.*
 import java.util.function.Supplier
 
+/**
+ * Class for defining a choreo autonomous routine with an event map
+ *
+ * @param pathGroup Name of the path group that is used for this routine
+ * @param swerve Swerve subsystem to send control requests to
+ * @param shouldReset Boolean to define if the auto routine should reset odometry to the initial position of the first path
+ * @param eventMap Map of path index -> command to run after path finishes, starts at zero
+ * @param timeout Timeout in seconds of the routine, defaults to 16, just above the auto period
+ */
 class ChoreoAuto(
     val pathGroup: String,
     val swerve: SwerveSubsystem,
-    val shouldReset: Boolean = false,
+    val shouldReset: Boolean = true,
     val eventMap: Map<Int, Supplier<Command>>,
     val timeout: Double = 16.0
 ) {
@@ -34,6 +44,6 @@ class ChoreoAuto(
             )
         }
 
-        return auto
+        return Commands.waitSeconds(timeout).deadlineWith(auto)
     }
 }
