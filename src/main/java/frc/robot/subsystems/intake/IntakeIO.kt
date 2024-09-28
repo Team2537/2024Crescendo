e754c19ea937a3f7f95dbe8e09e75ef8729b4a63
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-09-28 12:21:20 -0400

    Create IntakeIO.kt

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.kt
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.intake
+
+import org.littletonrobotics.junction.LogTable
+import org.littletonrobotics.junction.inputs.LoggableInputs
+
+interface IntakeIO {
+    class IntakeInputs : LoggableInputs {
+        var intakeLinearVelocity: Double = 0.0
+        var intakeSupplyVoltage: Double = 0.0
+        var intakeMotorVoltage: Double = 0.0
+        var intakeStatorCurrent: Double = 0.0
+        var exitSensor: Boolean = false
+        var intakeSensor: Boolean = false
+
+        /**
+         * Updates a LogTable with the data to log.
+         */
+        override fun toLog(table: LogTable) {
+            table.put("intakeLinearVelocity", intakeLinearVelocity)
+            table.put("intakeSupplyVoltage", intakeSupplyVoltage)
+            table.put("intakeMotorVoltage", intakeMotorVoltage)
+            table.put("intakeStatorCurrent", intakeStatorCurrent)
+            table.put("exitSensor", exitSensor)
+            table.put("intakeSensor", intakeSensor)
+        }
+
+        /**
+         * Updates data based on a LogTable.
+         */
+        override fun fromLog(table: LogTable) {
+            intakeLinearVelocity = table.get("intakeLinearVelocity", intakeLinearVelocity)
+            intakeSupplyVoltage = table.get("intakeSupplyVoltage", intakeSupplyVoltage)
+            intakeMotorVoltage = table.get("intakeMotorVoltage", intakeMotorVoltage)
+            intakeStatorCurrent = table.get("intakeStatorCurrent", intakeStatorCurrent)
+            exitSensor = table.get("exitSensor", exitSensor)
+            intakeSensor = table.get("intakeSensor", intakeSensor)
+        }
+    }
+    fun updateInputs(inputs: IntakeInputs) {}
+    fun applyVoltage(voltage: Double) {}
+    fun stop() {}
+}
