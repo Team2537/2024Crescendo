a0f72a6d0b545ed444267b939d73454e1080fcd5
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-09-28 12:21:22 -0400

    Create IntakeIONeo.kt

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIONeo.kt
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.intake
+
+import com.revrobotics.CANSparkBase
+import com.revrobotics.CANSparkLowLevel
+import com.revrobotics.CANSparkMax
+import edu.wpi.first.math.util.Units
+import edu.wpi.first.wpilibj.DigitalInput
+
+class IntakeIONeo(
+    private val motorID: Int,
+    private val intakeSensorID: Int,
+    private val exitSensorID: Int
+) : IntakeIO {
+
+    private val motor = CANSparkMax(motorID, CANSparkLowLevel.MotorType.kBrushless).apply {
+        restoreFactoryDefaults()
+        idleMode = CANSparkBase.IdleMode.kBrake
+        inverted = false
+        setSmartCurrentLimit(20)
+    }
+
+    private val exitSensor: DigitalInput = DigitalInput(exitSensorID)
+    private val intakeSensor: DigitalInput = DigitalInput(intakeSensorID)
+
+    override fun updateInputs(inputs: IntakeIO.IntakeInputs) {
+        inputs.intakeSensor = intakeSensor.get()
+        inputs.exitSensor = exitSensor.get()
+        inputs.intakeLinearVelocity = motor.encoder.velocity * (Units.inchesToMeters(0.84) / 60.0)
+        inputs.intakeSupplyVoltage = motor.busVoltage
+        inputs.intakeMotorVoltage = motor.appliedOutput * motor.busVoltage
+        inputs.intakeStatorCurrent = motor.outputCurrent
+    }
+
+    override fun applyVoltage(voltage: Double) {
+        motor.setVoltage(voltage)
+    }
+
+    override fun stop() {
+        motor.stopMotor()
+    }
+
+}
