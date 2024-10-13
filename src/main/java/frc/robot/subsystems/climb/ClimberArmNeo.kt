fc727f262b6d0f64db4f53ca8ea26743f0b2d146
falon <falon.n.clark@gmail.com>
2024-10-12 20:45:01 -0400

    Climb IO neo

--- src/main/java/frc/robot/subsystems/climb/ClimberArmNeo.kt
+++ src/main/java/frc/robot/subsystems/climb/ClimberArmNeo.kt
@@ -3,21 +3,33 @@ package frc.robot.subsystems.climb
 import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
 import edu.wpi.first.units.*
+import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.units.Units.Volts
+import lib.ControllerGains
+import lib.math.units.into
+import lib.math.units.rotations
+import lib.math.units.rpm
 
-class ClimberArmNeo(private val id: Int, private val isInverted: Boolean): ClimberArmIO {
+class ClimberArmNeo(
+    private val id: Int,
+    private val isInverted: Boolean,
+    private val gearing: Double,
+    private val gains: ControllerGains
+): ClimberArmIO {
     private val motor: CANSparkMax = CANSparkMax(id, CANSparkLowLevel.MotorType.kBrushless).apply {
         inverted = true
+        encoder.positionConversionFactor = 1/gearing
+        encoder.velocityConversionFactor = 1/gearing
+    }
+
+    override fun updateInputs(inputs: ClimberArmIO.ClimberArmInputs) {
+        inputs.velocity.mut_replace(motor.encoder.velocity.rpm)
+        inputs.relativePosition.mut_replace(motor.encoder.position.rotations)
+        inputs.appliedCurrent.mut_replace(Amps.of(motor.outputCurrent))
+        inputs.appliedVoltage.mut_replace(Volts.of(motor.appliedOutput * motor.busVoltage))
     }
 
     override fun setVoltage(voltage: Measure<Voltage>, isClosedLoop: Boolean) {
-        TODO("Not yet implemented")
-    }
-
-    override fun setMotorVelocity(velocity: Measure<Velocity<Angle>>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun setLinearVelocity(velocity: Measure<Velocity<Distance>>) {
-        TODO("Not yet implemented")
+        motor.setVoltage(voltage into Volts)
     }
 }
