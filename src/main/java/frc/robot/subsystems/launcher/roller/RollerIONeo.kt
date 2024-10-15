78b7286626f3d1f93605bbaa975108d647a37cde
falon <falon.n.clark@gmail.com>
2024-10-15 00:46:13 -0400

    neo roller

---
+++ src/main/java/frc/robot/subsystems/launcher/roller/RollerIONeo.kt
@@ -0,0 +1,58 @@
+package frc.robot.subsystems.launcher.roller
+
+import com.revrobotics.CANSparkBase
+import com.revrobotics.CANSparkLowLevel
+import com.revrobotics.CANSparkMax
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
+import edu.wpi.first.math.util.Units
+import edu.wpi.first.units.Distance
+import edu.wpi.first.units.Measure
+import edu.wpi.first.units.Units.*
+import edu.wpi.first.units.Voltage
+import lib.ControllerGains
+import lib.math.units.Rotation
+import lib.math.units.into
+
+class RollerIONeo(
+    private val id: Int,
+    private val isInverted: Boolean,
+    private val gains: ControllerGains,
+    private val rollerRadius: Measure<Distance>
+) : RollerIO {
+    private val motor: CANSparkMax = CANSparkMax(id, CANSparkLowLevel.MotorType.kBrushless).apply {
+        restoreFactoryDefaults()
+        inverted = isInverted
+
+        setSmartCurrentLimit(40)
+        pidController.setP(gains.kP)
+        pidController.setI(gains.kI)
+        pidController.setD(gains.kD)
+    }
+
+    private val feedforward: SimpleMotorFeedforward = SimpleMotorFeedforward(
+        gains.kS,
+        gains.kV,
+        gains.kA
+    )
+
+    override fun updateInputs(inputs: RollerIO.RollerInputs) {
+        inputs.velocity.mut_replace(motor.encoder.velocity, RPM)
+        inputs.position.mut_replace(motor.encoder.position, Rotations)
+        inputs.statorCurrent.mut_replace(motor.outputCurrent, Amps)
+        inputs.motorVoltage.mut_replace(motor.appliedOutput * motor.busVoltage, Volts)
+        inputs.supplyVoltage.mut_replace(motor.busVoltage, Volts)
+    }
+
+    override fun setVoltage(voltage: Measure<Voltage>, isClosedLoop: Boolean) {
+        motor.setVoltage(voltage into Volts)
+    }
+
+    override fun setTargetPosition(position: Measure<Distance>) {
+        val targetRotations = Units.radiansToRotations((position into Meters) / (rollerRadius into Meters))
+        motor.pidController.setReference(targetRotations, CANSparkBase.ControlType.kPosition)
+    }
+
+    override fun setBrakeMode(isBrakeMode: Boolean) {
+        motor.idleMode = if (isBrakeMode) CANSparkBase.IdleMode.kBrake else CANSparkBase.IdleMode.kCoast
+    }
+}
