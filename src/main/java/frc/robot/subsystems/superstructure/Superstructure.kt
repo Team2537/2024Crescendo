2fad05171f811e152cdb09c9e8ded6c313efd057
Europa <falon.n.clark@gmail.com>
2024-10-20 10:25:52 -0400

    Update at 'Sun Oct 20 10:25:51 EDT 2024'

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
@@ -81,7 +81,7 @@ class Superstructure {
 
     fun getConstantPullNote() =
         Commands.sequence(
-            roller.runOnce { roller.rollerIO.setVoltage(Units.Volts.of(1.0)) },
+            roller.runOnce { roller.rollerIO.setVoltage(Units.Volts.of(3.0)) },
             waitUntil(roller.isHoldingNote),
             waitSeconds(0.25),
             roller.getStopCommand()
